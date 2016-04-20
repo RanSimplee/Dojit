965cb77c7337a388475b50604fa0e40fee4f0b28
Ran Elkobi <ran@Simplee.com>
2016-04-20 12:34:22 +0300

    change comment into shallow nesting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,11 +19,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-
+    @topic = @post.topic
     authorize @comment
+    
     if @comment.destroy
       flash[:notice] = "Comment was removed."
       redirect_to [@topic, @post]
