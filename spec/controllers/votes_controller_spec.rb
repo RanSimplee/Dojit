4d5be66d520a21050730fc38f4ed54dd038e3ae8
Ran Elkobi <ran@Simplee.com>
2016-04-21 15:45:31 +0300

    added a test for down-vote in the votes controller

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -5,18 +5,29 @@ describe VotesController do
   include Devise::TestHelpers
   include TestFactories
 
-  describe '#up_vote' do
-    it "adds an up-vote to the post" do
-
+	before do
 			request.env["HTTP_REFERER"] = '/'
 	
       @user = authenticated_user
       @post = associated_post
       sign_in @user
+	end
+
+  describe '#up_vote' do
+    it "adds an up-vote to the post" do
 
       expect {
         post( :up_vote, post_id: @post.id )
       }.to change{ @post.up_votes }.by 1
+    end
+  end
+
+  describe '#down_vote' do
+    it "adds an down_vote to the post" do
+
+      expect {
+        post( :down_vote, post_id: @post.id )
+      }.to change{ @post.down_votes }.by 1
     end
   end
 end
