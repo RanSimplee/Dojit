baed4c05c8f44e294518f5c66ef8cc8126a74103
Ran Elkobi <ran@Simplee.com>
2016-04-26 18:36:03 +0300

    updated image size in post

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -24,7 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [300, 300]
+  process resize_to_fill: [640, 480]
 
   version :thumb do
     process resize_to_fill: [50, 50]
